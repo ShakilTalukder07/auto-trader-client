cf65947f2dc2736aef4a4566958ec0668ad01ddf
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-11-24 11:23:55 +0600

    firebase setup

---
+++ src/Pages/Shared/Blogs/Blogs.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <section className='mx-6'>
+            <div className='my-4'>
+                <p className='text-2xl font-bold my-2'>Q: What are the difference ways to manage a state in a react application?
+                </p>
+            </div>
+            <div className='my-4'>
+                <p className='text-2xl font-bold my-2'>Q: How does prototypical inheritance work?
+                </p>
+            </div>
+            <div className='my-4'>
+                <p className='text-2xl font-bold my-2'>Q: What is a unit test? Why should we write unit tests?
+                </p>
+            </div>
+            <div className='my-4'>
+                <p className='text-2xl font-bold my-2'>Q: What are the difference in React VS Angular VS Vue?
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default Blogs;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home/Home";
+import Blogs from "../../Pages/Shared/Blogs/Blogs";
 import DisplayError from "../../Pages/Shared/DisplayError/DisplayError";
 
 const router = createBrowserRouter([
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
             {
                 path:'/',
                 element:<Home></Home>
+            },
+            {
+                path:'/blogs',
+                element:<Blogs></Blogs>
             }
         ]
     }
