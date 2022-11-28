42b4dffb070b778c07e8fc8e002fa32503afd0e8
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-11-28 13:21:58 +0600

    payment section complete

--- src/Pages/Dashboard/AddAProduct/AddAProduct.js
+++ src/Pages/Dashboard/AddAProduct/AddAProduct.js
@@ -39,7 +39,7 @@ const AddAProduct = () => {
                         method: "POST",
                         headers: {
                             'content-type': 'application/json',
-                            authorization: `bearer${localStorage.getItem('accessToken')}`
+                            // authorization: `bearer${localStorage.getItem('accessToken')}`
                         },
                         body: JSON.stringify(products)
                     })
--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,13 +1,30 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 
 const Payment = () => {
 
-    const data = useLoaderData()
-    console.log(data);
+    const product = useLoaderData()
+
+    const { itemName, itemPrice } = product
+
     return (
         <div>
-            <h3 className="text-3xl font-bold">Make Payment</h3>
+            <h3 className="text-3xl font-bold mt-5 ml-4">Make Payment for: {itemName}</h3>
+            <p className='text-xl mt-5 ml-4'>Please pay <strong>${itemPrice}</strong> for confirm your order.</p>
+            <div className='card w-96 bg-base-100 shadow-xl my-20 ml-4'>
+                <div className="card-body">
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm
+                            product={product}
+                        ></CheckoutForm>
+                    </Elements>
+                </div>
+            </div>
         </div>
     );
 };
